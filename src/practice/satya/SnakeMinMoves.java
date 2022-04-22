e6121591239bf9646b2d25a9888eed220146f87d
Satya Sri <satyasripulipati@gmail.com>
2022-04-22 20:58:45 +0530

    Updated head in printing logic

--- src/practice/satya/SnakeMinMoves.java
+++ src/practice/satya/SnakeMinMoves.java
@@ -16,13 +16,20 @@ public class SnakeMinMoves {
 
     public int minimumMoves(int[][] grid) {
         int res = 0;
+        SnakePos.setGrid(grid);
         SnakePos initialPos = new SnakePos(0, 0, false);
-        initialPos.printGrid(grid);
+        initialPos.printGrid();
         return res;
     }
 }
 
 class SnakePos {
+    static int[][] grid;
+
+    public static void setGrid(int[][] grid) {
+        SnakePos.grid = grid;
+    }
+
     int x, y; // (x,y) are position of tail// so head will be in (x+1,y) when isVertical is true else (x,y+1)
     boolean isVertical; // = true
 
@@ -32,11 +39,20 @@ class SnakePos {
         this.isVertical = isVertical;
     }
 
-    public void printGrid(int[][] grid) {
+    public void printGrid() {
+        int hx = x, hy = y;
+        if (isVertical)
+            hx++;
+        else
+            hy++;
+
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (i == x && j == y) {
                     System.out.print("T" + " ");
+                } else
+                if (i == hx && j == hy) {
+                    System.out.print("H" + " ");
                 } else
                     System.out.print(grid[i][j] + " ");
             }
