9d9844c2280ff8a3f9b7971b945e8e2984c0bfa6
Satya Sri <satyasripulipati@gmail.com>
2022-04-22 22:45:32 +0530

    Implemented logic to find min moves

--- src/practice/satya/SnakeMinMoves.java
+++ src/practice/satya/SnakeMinMoves.java
@@ -1,5 +1,9 @@
 package practice.satya;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
 public class SnakeMinMoves {
     public static void main(String[] args) {
         int[][] grid = new int[][]{
@@ -11,23 +15,39 @@ public class SnakeMinMoves {
                 {0, 1, 1, 0, 0, 0}};
         SnakeMinMoves snakeMinMoves = new SnakeMinMoves();
 
-        snakeMinMoves.minimumMoves(grid);
+        System.out.println(snakeMinMoves.minimumMoves(grid));
     }
 
     public int minimumMoves(int[][] grid) {
-        int res = 0;
+        int res = -1;
         SnakePos.setGrid(grid);
-        SnakePos initialPos = new SnakePos(0, 0, false);
-        initialPos.printGrid();
-        SnakePos next;
-        if((next=initialPos.moveAntiClockWise())!=null)
-            next.printGrid();
-        if((next=initialPos.moveClockWise())!=null)
-            next.printGrid();
-        if((next=initialPos.moveHorizontal())!=null)
-            next.printGrid();
-        if((next=initialPos.moveVertical())!=null)
-            next.printGrid();
+        SnakePos initialPos = new SnakePos(0, 0, false, 0);
+        SnakePos targetPos = new SnakePos(grid.length - 1, grid[grid.length - 1].length - 2, false);
+        //initialPos.printGrid();
+        boolean[][][] visited = new boolean[grid.length][grid[0].length][2];
+        SnakePos next, cur;
+        Queue<SnakePos> queue = new LinkedList<>();
+        queue.add(initialPos);
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            if (cur.equals(targetPos))
+                return cur.d;
+            if (visited[cur.x][cur.y][cur.isVertical ? 1 : 0])
+                continue;
+            visited[cur.x][cur.y][cur.isVertical ? 1 : 0] = true;
+            if ((next = cur.moveAntiClockWise()) != null) {
+                queue.add(next);
+            }
+            if ((next = cur.moveClockWise()) != null) {
+                queue.add(next);
+            }
+            if ((next = cur.moveHorizontal()) != null) {
+                queue.add(next);
+            }
+            if ((next = cur.moveVertical()) != null) {
+                queue.add(next);
+            }
+        }
 
         return res;
     }
@@ -35,6 +55,33 @@ public class SnakeMinMoves {
 
 class SnakePos {
     static int[][] grid;
+    int d = Integer.MAX_VALUE;
+
+    public SnakePos(int x, int y, boolean isVertical, int d) {
+        this.d = d;
+        this.x = x;
+        this.y = y;
+        this.isVertical = isVertical;
+        //System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "SnakePos{" +
+                "d=" + d +
+                ", x=" + x +
+                ", y=" + y +
+                ", isVertical=" + isVertical +
+                '}';
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
 
     public static void setGrid(int[][] grid) {
         SnakePos.grid = grid;
@@ -75,13 +122,13 @@ class SnakePos {
             int headY = y + 1;
             int nextY = headY + 1;
             if (isValidMove(x, nextY))
-                return new SnakePos(x, headY, false);
+                return new SnakePos(x, headY, false, d + 1);
             else
                 return null;
         } else {
             int nextY = y + 1;
-            if (isValidMove(x, nextY) && isValidMove(x+1, nextY))
-                return new SnakePos(x, nextY, true);
+            if (isValidMove(x, nextY) && isValidMove(x + 1, nextY))
+                return new SnakePos(x, nextY, true, d + 1);
             else
                 return null;
         }
@@ -92,13 +139,13 @@ class SnakePos {
             int headX = x + 1;
             int nextX = headX + 1;
             if (isValidMove(nextX, y)) {
-                return new SnakePos(headX, y, true);
+                return new SnakePos(headX, y, true, d + 1);
             } else
                 return null;
         } else {
             int nextX = x + 1;
             if (isValidMove(nextX, y) && isValidMove(nextX, y + 1)) {
-                return new SnakePos(nextX, y, false);
+                return new SnakePos(nextX, y, false, d + 1);
             } else return null;
         }
     }
@@ -108,7 +155,7 @@ class SnakePos {
             return null;
         int nextX = x + 1;
         if (isValidMove(nextX, y) && isValidMove(nextX, y + 1)) {
-            return new SnakePos(x, y, true);
+            return new SnakePos(x, y, true, d + 1);
         } else
             return null;
     }
@@ -118,7 +165,7 @@ class SnakePos {
             return null;
         int nextY = y + 1;
         if (isValidMove(x, nextY) && isValidMove(x + 1, nextY)) {
-            return new SnakePos(x, y, false);
+            return new SnakePos(x, y, false, d + 1);
         } else
             return null;
     }
@@ -127,5 +174,18 @@ class SnakePos {
         if (x >= 0 && x < grid.length && y >= 0 && y < grid[x].length && grid[x][y] == 0)
             return true;
         return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SnakePos snakePos = (SnakePos) o;
+        return x == snakePos.x && y == snakePos.y && isVertical == snakePos.isVertical;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, isVertical);
     }
 }
